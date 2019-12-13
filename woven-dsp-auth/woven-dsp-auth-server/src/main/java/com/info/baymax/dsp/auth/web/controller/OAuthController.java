fc8f7b8d242f83ec80bb79d0b18516bd88098b31
760374564 <760374564@qq.com>
2019-12-13 16:37:27 +0800

    调整包名

--- woven-dsp-auth/woven-dsp-auth-server/src/main/java/com/info/baymax/dsp/auth/web/controller/OAuthController.java
+++ woven-dsp-auth/woven-dsp-auth-server/src/main/java/com/info/baymax/dsp/auth/web/controller/OAuthController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -13,6 +14,7 @@ import io.swagger.annotations.ApiOperation;
 
 @Api(tags = "认证管理", value = "用户认证相关接口", description = "系统用户认证管理")
 @RestController
+@RequestMapping("/oauth")
 @SessionAttributes("authorizationRequest")
 public class OAuthController {
 
