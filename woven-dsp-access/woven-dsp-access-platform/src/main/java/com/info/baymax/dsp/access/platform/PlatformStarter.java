9e19ae7dfa8d3b7dfbc634eb4ae4c57b99a81171
wuguofeng <guofeng.wu@inforefiner.com>
2019-12-23 12:25:51 +0800

    update

--- woven-dsp-access/woven-dsp-access-platform/src/main/java/com/info/baymax/dsp/access/platform/PlatformStarter.java
+++ woven-dsp-access/woven-dsp-access-platform/src/main/java/com/info/baymax/dsp/access/platform/PlatformStarter.java
@@ -1,6 +1,8 @@
 package com.info.baymax.dsp.access.platform;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticSearchRestHealthIndicatorAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -16,6 +18,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EntityScan(basePackages = {"com.info.baymax.dsp.data.**.entity"})
 @MapperScan(basePackages = "com.info.baymax.dsp.data.**.mapper")
 @EnableExtProperties("classpath*:**/access-platform*.properties")
+@EnableAutoConfiguration(exclude = {ElasticSearchRestHealthIndicatorAutoConfiguration.class})
 public class PlatformStarter {
 
     public static void main(String[] args) {
