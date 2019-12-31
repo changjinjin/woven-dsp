e57423d75043ad0d0383f07a99351aa1ba429804
navywang110 <haijun.love@foxmail.com>
2019-12-31 16:26:00 +0800

    FieldMapping for DataResource

---
+++ woven-dsp-data/woven-dsp-data-platform/src/main/java/com/info/baymax/dsp/data/platform/mybatis/mapper/type/GZBase64ClobVsListFieldMappingTypeHandler.java
@@ -0,0 +1,9 @@
+package com.info.baymax.dsp.data.platform.mybatis.mapper.type;
+
+
+import com.info.baymax.common.mybatis.type.base64.clob.GZBase64ClobVsListTypeHandler;
+import com.info.baymax.dsp.data.dataset.entity.core.DataField;
+import com.info.baymax.dsp.data.platform.bean.FieldMapping;
+
+public class GZBase64ClobVsListFieldMappingTypeHandler extends GZBase64ClobVsListTypeHandler<FieldMapping> {
+}
