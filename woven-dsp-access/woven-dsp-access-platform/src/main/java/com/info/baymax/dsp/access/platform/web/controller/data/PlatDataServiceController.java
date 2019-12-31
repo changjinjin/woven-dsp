5e88a5b9ac8e9fbbff5448ddc5079058b6f63213
760374564 <760374564@qq.com>
2019-12-31 11:32:18 +0800

    添加数据服务启用和停用接口

--- woven-dsp-access/woven-dsp-access-platform/src/main/java/com/info/baymax/dsp/access/platform/web/controller/data/PlatDataServiceController.java
+++ woven-dsp-access/woven-dsp-access-platform/src/main/java/com/info/baymax/dsp/access/platform/web/controller/data/PlatDataServiceController.java
@@ -2,13 +2,17 @@ package com.info.baymax.dsp.access.platform.web.controller.data;
 
 import com.info.baymax.common.comp.base.BaseEntityController;
 import com.info.baymax.common.entity.base.BaseEntityService;
+import com.info.baymax.common.message.exception.ControllerException;
+import com.info.baymax.common.message.result.ErrType;
+import com.info.baymax.common.message.result.Response;
 import com.info.baymax.dsp.data.platform.entity.DataService;
 import com.info.baymax.dsp.data.platform.service.DataServiceEntityService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,5 +26,16 @@ public class PlatDataServiceController implements BaseEntityController<DataServi
     @Override
     public BaseEntityService<DataService> getBaseEntityService() {
         return dataServiceEntityService;
+    }
+
+    @ApiOperation(value = "启用停用", notes = "服务启用停用接口")
+    @GetMapping("/updateStatus")
+    @ResponseBody
+    public Response<?> updateStatus(@ApiParam(value = "启用停用对象，传ID和状态值", required = true) @RequestBody DataService t) {
+        if (t == null) {
+            throw new ControllerException(ErrType.BAD_REQUEST, "查询记录ID不能为空");
+        }
+        dataServiceEntityService.saveOrUpdate(t);
+        return Response.ok();
     }
 }
