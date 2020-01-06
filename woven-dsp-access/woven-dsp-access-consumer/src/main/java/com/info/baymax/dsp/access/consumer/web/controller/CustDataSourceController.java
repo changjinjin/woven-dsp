db0d060258311562fa5f319b1a13839e486070d0
navywang110 <haijun.love@foxmail.com>
2020-01-06 12:26:35 +0800

    update query fieldGroup

--- woven-dsp-access/woven-dsp-access-consumer/src/main/java/com/info/baymax/dsp/access/consumer/web/controller/CustDataSourceController.java
+++ woven-dsp-access/woven-dsp-access-consumer/src/main/java/com/info/baymax/dsp/access/consumer/web/controller/CustDataSourceController.java
@@ -9,6 +9,7 @@ import com.info.baymax.common.message.result.Response;
 import com.info.baymax.common.mybatis.page.IPage;
 import com.info.baymax.common.saas.SaasContext;
 import com.info.baymax.common.service.criteria.example.ExampleQuery;
+import com.info.baymax.common.service.criteria.example.FieldGroup;
 import com.info.baymax.dsp.data.consumer.beans.source.CheckEntity;
 import com.info.baymax.dsp.data.consumer.entity.CustDataSource;
 import com.info.baymax.dsp.data.consumer.service.CustDataSourceService;
@@ -60,8 +61,12 @@ public class CustDataSourceController implements BaseEntityController<CustDataSo
 
     @Override
     public Response<IPage<CustDataSource>> page(ExampleQuery query) {
-        return BaseEntityController.super.page(
-            ExampleQuery.builder(query).fieldGroup().andEqualTo("owner", SaasContext.getCurrentUserId()).end());
+        query = ExampleQuery.builder(query);
+        if(query.getFieldGroup() == null){
+            query.setFieldGroup(new FieldGroup());
+        }
+        query.getFieldGroup().andEqualTo("owner", SaasContext.getCurrentUserId());
+        return BaseEntityController.super.page(query);
     }
 
     @ApiOperation(value = "查询数据库驱动信息", notes = "如果是JDBC类型的数据源，查询系统内置的数据库驱动信息用于用户创建数据源")
