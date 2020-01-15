ac7253fbd9004d926a4ae1fded93ef1f903c9f8a
navywang110 <haijun.love@foxmail.com>
2020-01-15 15:38:17 +0800

    support api for execution list

--- woven-dsp-access/woven-dsp-access-platform/src/main/java/com/info/baymax/dsp/access/platform/web/controller/data/PlatDataServiceController.java
+++ woven-dsp-access/woven-dsp-access-platform/src/main/java/com/info/baymax/dsp/access/platform/web/controller/data/PlatDataServiceController.java
@@ -2,16 +2,25 @@ package com.info.baymax.dsp.access.platform.web.controller.data;
 
 import com.info.baymax.common.comp.base.BaseEntityController;
 import com.info.baymax.common.entity.base.BaseEntityService;
+import com.info.baymax.common.jpa.criteria.query.QueryObject;
 import com.info.baymax.common.message.exception.ControllerException;
 import com.info.baymax.common.message.result.ErrType;
 import com.info.baymax.common.message.result.Response;
+import com.info.baymax.common.mybatis.page.IPage;
+import com.info.baymax.common.saas.SaasContext;
+import com.info.baymax.common.service.criteria.example.ExampleQuery;
+import com.info.baymax.common.service.criteria.example.FieldGroup;
 import com.info.baymax.dsp.data.consumer.constant.DataServiceType;
+import com.info.baymax.dsp.data.dataset.entity.core.Dataset;
+import com.info.baymax.dsp.data.dataset.entity.core.FlowExecution;
+import com.info.baymax.dsp.data.dataset.service.core.FlowExecutionService;
 import com.info.baymax.dsp.data.platform.entity.DataService;
 import com.info.baymax.dsp.data.platform.service.DataServiceEntityService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +31,10 @@ import org.springframework.web.bind.annotation.*;
 public class PlatDataServiceController implements BaseEntityController<DataService> {
 
     @Autowired
-    DataServiceEntityService dataServiceEntityService;
+    private DataServiceEntityService dataServiceEntityService;
+
+    @Autowired
+    private FlowExecutionService flowExecutionService;
 
     @Override
     public BaseEntityService<DataService> getBaseEntityService() {
@@ -66,5 +78,21 @@ public class PlatDataServiceController implements BaseEntityController<DataServi
         }
 
         return Response.ok(dataService);
+    }
+
+    @ApiOperation(value = "查找Execution", notes = "多条件查询Execution")
+    @ResponseBody
+    @GetMapping("/tasklist/{flowId}")
+    public Response<IPage<FlowExecution>> query(@PathVariable String flowId) {
+        if (StringUtils.isEmpty(flowId)) {
+            throw new ControllerException(ErrType.BAD_REQUEST, "查询条件不能为空");
+        }
+        ExampleQuery query = ExampleQuery.builder(FlowExecution.class);
+        if(query.getFieldGroup() == null){
+            query.setFieldGroup(new FieldGroup());
+        }
+        query.getFieldGroup().andEqualTo("flowId", flowId);
+        query.getFieldGroup().andEqualTo("tenantId", SaasContext.getCurrentTenantId());
+        return Response.ok(flowExecutionService.selectPage(query));
     }
 }
