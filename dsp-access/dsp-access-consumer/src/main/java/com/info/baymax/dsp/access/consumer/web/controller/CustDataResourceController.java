b7770d877718f801fba08a55ff1c1b3dd5d66d38
navywang110 <haijun.love@foxmail.com>
2020-02-05 16:25:39 +0800

    support query detail for DataResource

--- dsp-access/dsp-access-consumer/src/main/java/com/info/baymax/dsp/access/consumer/web/controller/CustDataResourceController.java
+++ dsp-access/dsp-access-consumer/src/main/java/com/info/baymax/dsp/access/consumer/web/controller/CustDataResourceController.java
@@ -1,20 +1,21 @@
 package com.info.baymax.dsp.access.consumer.web.controller;
 
+import com.info.baymax.common.jpa.criteria.query.QueryObject;
+import com.info.baymax.common.message.exception.ControllerException;
+import com.info.baymax.common.message.result.ErrType;
 import com.info.baymax.common.message.result.Response;
 import com.info.baymax.common.page.IPage;
 import com.info.baymax.common.saas.SaasContext;
 import com.info.baymax.common.service.criteria.example.ExampleQuery;
 import com.info.baymax.common.service.criteria.example.FieldGroup;
+import com.info.baymax.dsp.data.dataset.entity.core.Dataset;
 import com.info.baymax.dsp.data.platform.entity.DataResource;
 import com.info.baymax.dsp.data.platform.service.DataResourceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "消费端：数据资源查询相关接口", description = "数据资源查询相关接口")
 @RestController
@@ -31,5 +32,15 @@ public class CustDataResourceController {
         }
         query.getFieldGroup().andEqualTo("tenantId", SaasContext.getCurrentTenantId()).andEqualTo("openStatus", 1);
         return Response.ok(dataResourceService.selectPage(query));
+    }
+
+    @ApiOperation(value = "查询详情", notes = "根据ID查询单条数据的详情，ID不能为空")
+    @GetMapping("/infoById")
+    @ResponseBody
+    public Response<DataResource> infoById(@ApiParam(value = "记录ID", required = true) @RequestParam Long id) {
+        if (id == null) {
+            throw new ControllerException(ErrType.BAD_REQUEST, "查询记录ID不能为空");
+        }
+        return Response.ok(dataResourceService.selectByPrimaryKey(id));
     }
 }
