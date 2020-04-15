398b98a51c11410626d5bd996cbb18e3ddfef657
760374564 <760374564@qq.com>
2020-04-15 16:43:44 +0800

    添加文档注释

--- dsp-access/dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/request/PullRequest.java
+++ dsp-access/dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/request/PullRequest.java
@@ -1,15 +1,22 @@
 package com.info.baymax.dsp.access.dataapi.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel
 public class PullRequest implements Serializable {
 	private static final long serialVersionUID = 945291461084587382L;
 
+	@ApiModelProperty("服务ID")
 	private String dataServiceId;
+	@ApiModelProperty("应用接入key")
 	private String accessKey;
+	@ApiModelProperty("数据偏移游标")
 	private int offset;
+	@ApiModelProperty("请求数据量")
 	private int size;
 }
