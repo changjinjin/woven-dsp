65e12143575404dea5697a2cc186363d92d3bd34
760374564 <760374564@qq.com>
2020-04-15 13:21:38 +0800

    update startup shell script

--- dsp-distribution/bin/auth-server.sh
+++ dsp-distribution/bin/auth-server.sh
@@ -16,9 +16,9 @@ WOVEN_COM_NAME=auth-server
 checkAction "${action}" "${WOVEN_COM_NAME}.sh"
 
 PID_FILE="${WOVEN_COM_NAME}.pid"
-#JVM_OPTS="${JVM_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8403"
-JVM_OPTS="${JVM_OPTS} -DWOVEN_COM_NAME=${WOVEN_COM_NAME} -Dfile.encoding=UTF-8 -DWOVEN_APP=woven -DWOVEN_LOGS_DIR=${WOVEN_LOGS_DIR} -DKAFKA_BROKER_LIST=${KAFKA_BROKER_LIST} -Dgrpc.server.address=${GRPC_WOVEN_DFEXECUTOR_ADDR} -Dgrpc.server.port=${GRPC_WOVEN_DFEXECUTOR_PORT} ${AUTH_SERVER_JVM_OPTS}"
-JVM_OPTS="${JVM_OPTS} -classpath ${WOVEN_CONF}:${YARN_CONF_DIR}:lib/*:${SPARK_HOME}/jars/*:${ALL_EXTRA_CLASSPATH:-.} com.info.baymax.dsp.auth.AuthStarter"
+#JVM_OPTS="${JVM_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8604"
+JVM_OPTS="${JVM_OPTS} -DWOVEN_COM_NAME=${WOVEN_COM_NAME} -Dfile.encoding=UTF-8 -DWOVEN_APP=woven -DWOVEN_LOGS_DIR=${WOVEN_LOGS_DIR} ${AUTH_SERVER_JVM_OPTS}"
+JVM_OPTS="${JVM_OPTS} -classpath ${WOVEN_CONF}:${YARN_CONF_DIR}:lib/*:${ALL_EXTRA_CLASSPATH:-.} com.info.baymax.dsp.auth.AuthStarter"
 
 APP_CMD="${JAVA_CMD} ${JVM_OPTS}"
 WORK_DIR="${WOVEN_HOME}/${WOVEN_COM_NAME}"
