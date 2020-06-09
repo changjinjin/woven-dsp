bc3ca9da122b29b153a37c26ef8659a21df4ea7c
760374564 <760374564@qq.com>
2020-06-09 18:23:11 +0800

    add sentinel rule logic

--- dsp-gateway/src/main/java/com/info/baymax/dsp/gateway/DspGatewayStarter.java
+++ dsp-gateway/src/main/java/com/info/baymax/dsp/gateway/DspGatewayStarter.java
@@ -7,14 +7,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
-import springfox.documentation.spring.web.plugins.DocumentationPluginsBootstrapper;
 
 @SpringCloudApplication
-@ComponentScan(basePackages = {
-    "com.info.baymax"}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
-    DocumentationPluginsBootstrapper.class}))
+@ComponentScan(basePackages = {"com.info.baymax"})
 @EnableAutoConfiguration
 @PropertySource(value = {"classpath:/dsp-gateway.yml"}, factory = YamlPropertySourceFactory.class)
 public class DspGatewayStarter {
---
+++ dsp-gateway/src/main/java/com/info/baymax/dsp/gateway/config/sentinel/SentinelConfiguration.java
@@ -0,0 +1,13 @@
+package com.info.baymax.dsp.gateway.config.sentinel;
+
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SentinelConfiguration {
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
+    }
+}
