6d37682e24e29b1bebd9c5f608df3570d1a71203
760374564 <760374564@qq.com>
2020-07-06 17:40:11 +0800

    modify es sql parse logic

--- dsp-access/dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/web/controller/DataApiController.java
+++ dsp-access/dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/web/controller/DataApiController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.Serializable;
