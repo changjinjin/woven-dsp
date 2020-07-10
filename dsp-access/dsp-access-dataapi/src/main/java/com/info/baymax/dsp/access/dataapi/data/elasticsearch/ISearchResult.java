090e402ea3eadf832b13dc58615f5c1b7cb3cc65
760374564 <760374564@qq.com>
2020-07-10 20:33:46 +0800

    fix sql assembly error

--- dsp-access/dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/data/elasticsearch/ISearchResult.java
+++ dsp-access/dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/data/elasticsearch/ISearchResult.java
@@ -1,6 +1,9 @@
 package com.info.baymax.dsp.access.dataapi.data.elasticsearch;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
 import io.searchbox.core.SearchResult;
 
 // fix jest issues: "https://github.com/searchbox-io/Jest/issues/656"
@@ -13,18 +16,14 @@ public class ISearchResult extends SearchResult {
     @Override
     public Long getTotal() {
         Long total = null;
-        JsonObject obj = (JsonObject) getPath(PATH_TO_TOTAL);
-        if (obj != null)
-            total = obj.getAsJsonPrimitive("value").getAsLong();
+        JsonElement element = getPath(PATH_TO_TOTAL);
+        if (element != null) {
+            if (element instanceof JsonPrimitive) {
+                return ((JsonPrimitive) element).getAsLong();
+            } else if (element instanceof JsonObject) {
+                total = ((JsonObject) element).getAsJsonPrimitive("value").getAsLong();
+            }
+        }
         return total;
-    }
-
-    @Override
-    public Float getMaxScore() {
-        Float maxScore = null;
-        JsonObject obj = (JsonObject) getPath(PATH_TO_MAX_SCORE);
-        if (obj != null && !obj.isJsonNull())
-            maxScore = obj.getAsJsonPrimitive("value").getAsFloat();
-        return maxScore;
     }
 }
---
+++ dsp-access/dsp-access-dataapi/src/test/java/com/info/baymax/dsp/access/dataapi/client/JestClientTest.java
@@ -0,0 +1,59 @@
+package com.info.baymax.dsp.access.dataapi.client;
+
+import com.info.baymax.dsp.access.dataapi.config.jest.JestClientUtils;
+import com.info.baymax.dsp.access.dataapi.config.jest.JestConf;
+import com.info.baymax.dsp.access.dataapi.data.elasticsearch.ISearchResult;
+import com.inforefiner.repackaged.org.apache.curator.shaded.com.google.common.collect.Lists;
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class JestClientTest {
+
+    @Test
+    public void testEs5() throws IOException {
+        String url = "http://192.168.1.85:9203";
+        String indexName = "sink_es";
+        String indexType = "sink_es";
+        search(url, indexName, indexType);
+    }
+
+    @Test
+    public void testEs6() throws IOException {
+        String url = "http://192.168.2.145:9201";
+        String indexName = "commodity";
+        String indexType = "commodity";
+        search(url, indexName, indexType);
+    }
+
+    @Test
+    public void testEs7() throws IOException {
+        // String url = "http://192.168.2.145:9201";
+        String url = "http://192.168.2.145:9200";
+        String indexName = "commodity";
+        String indexType = "commodity";
+        search(url, indexName, indexType);
+    }
+
+    private void search(String url, String indexName, String indexType) throws IOException {
+        JestClient jestClient = jestClient(url);
+        String query = QueryBuilders.matchAllQuery().toString().trim();
+        System.out.println("query:\r\n" + query);
+        SearchResult result = new ISearchResult(
+            jestClient.execute(new Search.Builder(query).addIndex(indexName).addType(indexType).build()));
+        if (result.isSucceeded()) {
+            System.out.println("result to string:" + result.getJsonString());
+            System.out.println("total hits:" + result.getTotal());
+        } else {
+            System.out.println("ErrorMessage:" + result.getErrorMessage());
+        }
+    }
+
+    private JestClient jestClient(String url) {
+        return JestClientUtils.jestClient(JestConf.builder().uris(Lists.newArrayList(url)).build());
+    }
+}
