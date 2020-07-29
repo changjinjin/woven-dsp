3897fe00a6541bea6a00cf2569e4fccf4cbccbbc
760374564 <760374564@qq.com>
2020-07-29 14:45:11 +0800

    fix compile error

--- dsp-common/dsp-common-webmvc/src/main/java/com/info/baymax/dsp/common/webmvc/filter/ServletSaasContextWebFilter.java
+++ dsp-common/dsp-common-webmvc/src/main/java/com/info/baymax/dsp/common/webmvc/filter/ServletSaasContextWebFilter.java
@@ -1,17 +1,24 @@
 package com.info.baymax.dsp.common.webmvc.filter;
 
-import com.alibaba.fastjson.JSON;
-import com.info.baymax.common.saas.SaasContext;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.info.baymax.common.saas.SaasContext;
+import com.info.baymax.common.utils.JsonUtils;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * SaasContext 拦截初始化
@@ -23,25 +30,25 @@ import java.net.URLDecoder;
 @Component
 public class ServletSaasContextWebFilter implements Filter {
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
-        throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String saasContextHeader = request.getHeader(SaasContext.SAAS_CONTEXT_KEY);
-        if (StringUtils.isNotEmpty(saasContextHeader)) {
-            String decode = null;
-            try {
-                decode = URLDecoder.decode(saasContextHeader, "UTF-8");
-                SaasContext saasContext = JSON.parseObject(decode, SaasContext.class);
-                if (saasContext != null) {
-                    SaasContext.setCurrentSaasContext(saasContext);
-                }
-                log.debug("fix current SaasContext :{}", decode);
-            } catch (UnsupportedEncodingException e) {
-                log.error(e.getMessage(), e);
-            }
-        }
-        chain.doFilter(request, response);
-    }
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		String saasContextHeader = request.getHeader(SaasContext.SAAS_CONTEXT_KEY);
+		if (StringUtils.isNotEmpty(saasContextHeader)) {
+			String decode = null;
+			try {
+				decode = URLDecoder.decode(saasContextHeader, "UTF-8");
+				SaasContext saasContext = JsonUtils.fromJson(decode, SaasContext.class);
+				if (saasContext != null) {
+					SaasContext.setCurrentSaasContext(saasContext);
+				}
+				log.debug("fix current SaasContext :{}", decode);
+			} catch (UnsupportedEncodingException e) {
+				log.error(e.getMessage(), e);
+			}
+		}
+		chain.doFilter(request, response);
+	}
 }
