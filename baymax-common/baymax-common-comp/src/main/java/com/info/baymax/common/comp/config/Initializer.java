3ad8c43d925010d0ea28eb11242b71442e31ca9e
760374564 <760374564@qq.com>
2020-11-10 14:41:14 +0800

    add Initializer for some things init

---
+++ baymax-common/baymax-common-comp/src/main/java/com/info/baymax/common/comp/config/Initializer.java
@@ -0,0 +1,13 @@
+package com.info.baymax.common.comp.config;
+
+/**
+ * 信息初始化信息执行器
+ *
+ * @author jingwei.yang
+ * @date 2020年11月10日 下午12:27:42
+ */
+public interface Initializer {
+
+    void init();
+
+}
