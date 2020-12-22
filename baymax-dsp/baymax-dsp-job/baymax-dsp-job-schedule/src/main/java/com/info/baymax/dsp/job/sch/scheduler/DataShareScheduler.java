b4b8f86b20330418e68747784a0bd1075cefbf6a
760374564 <760374564@qq.com>
2020-12-22 19:17:18 +0800

    refactor(job-schedule): change log level from error to warn
    change log level from error to warn

--- baymax-dsp/baymax-dsp-job/baymax-dsp-job-schedule/src/main/java/com/info/baymax/dsp/job/sch/scheduler/DataShareScheduler.java
+++ baymax-dsp/baymax-dsp-job/baymax-dsp-job-schedule/src/main/java/com/info/baymax/dsp/job/sch/scheduler/DataShareScheduler.java
@@ -1,30 +1,17 @@
 package com.info.baymax.dsp.job.sch.scheduler;
 
-import java.util.Date;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-import org.apache.commons.lang.StringUtils;
-import org.quartz.CronScheduleBuilder;
-import org.quartz.Job;
-import org.quartz.JobBuilder;
-import org.quartz.JobDataMap;
-import org.quartz.JobDetail;
-import org.quartz.JobKey;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.TriggerKey;
-import org.quartz.impl.StdSchedulerFactory;
-import org.springframework.stereotype.Component;
-
 import com.info.baymax.common.utils.JsonUtils;
 import com.info.baymax.dsp.data.consumer.constant.ScheduleType;
 import com.info.baymax.dsp.data.platform.entity.DataService;
-
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Date;
 
 /**
  * @Author: haijun
@@ -90,9 +77,9 @@ public class DataShareScheduler implements AbstractScheduler<DataService> {
                 Long startTime = Long.parseLong(ds.getServiceConfiguration().get("startTime"));
                 startMiss =  startTime;
             } catch (NumberFormatException ex) {
-                log.error("NumberFormatException: For input parameter startTime : {}", ds.getServiceConfiguration().get("startTime"));
+                log.warn("NumberFormatException: For input parameter startTime : {}", ds.getServiceConfiguration().get("startTime"));
             } catch (NullPointerException ex) {
-                log.error("NullPointException: : For input parameter not found startTime");
+                log.warn("NullPointException: : For input parameter not found startTime");
             }
 
             if (ds.getLastExecutedTime() != null && ds.getLastExecutedTime().getTime() > startMiss) {
