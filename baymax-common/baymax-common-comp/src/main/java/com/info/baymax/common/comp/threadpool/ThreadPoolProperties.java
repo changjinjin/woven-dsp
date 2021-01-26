56c9bcd3f028e6d12e702b67db8573cb22f7e77f
760374564 <760374564@qq.com>
2021-01-26 19:28:22 +0800

    feat(common): add thread pool properties
    add thread pool properties

---
+++ baymax-common/baymax-common-comp/src/main/java/com/info/baymax/common/comp/threadpool/ThreadPoolProperties.java
@@ -0,0 +1,17 @@
+package com.info.baymax.common.comp.threadpool;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Setter
+@Getter
+@ConfigurationProperties(prefix = ThreadPoolProperties.PREFIX)
+public class ThreadPoolProperties {
+    public static final String PREFIX = "thread.pool";
+
+    private int corePoolSize = 5;
+    private int maxPoolSize = 20;
+    private int queueCapacity = 99999;
+    private String threadNamePrefix = "thread-pool-";
+}
--- baymax-common/baymax-common-comp/src/main/java/com/info/baymax/common/comp/threadpool/ThreadPoolTaskExecutorConfig.java
+++ baymax-common/baymax-common-comp/src/main/java/com/info/baymax/common/comp/threadpool/ThreadPoolTaskExecutorConfig.java
@@ -1,6 +1,8 @@
 package com.info.baymax.common.comp.threadpool;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -15,25 +17,25 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @Slf4j
 @Configuration
+@EnableConfigurationProperties(value = ThreadPoolProperties.class)
 public class ThreadPoolTaskExecutorConfig {
 
-    private final int corePoolSize = 5;
-    private final int maxPoolSize = 20;
-    private final int queueCapacity = 99999;
-    private final String threadNamePrefix = "custom-pool-thread-";
+    @Autowired
+    private ThreadPoolProperties poolProperties;
 
     @Bean
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
         if (log.isInfoEnabled()) {
             log.info(
                 "Start a new ThreadPoolTaskExecutor: threadNamePrefix [{}],corePoolSize [{}],maxPoolSize [{}],queueCapacity [{}].",
-                threadNamePrefix, corePoolSize, maxPoolSize, queueCapacity);
+                poolProperties.getThreadNamePrefix(), poolProperties.getCorePoolSize(),
+                poolProperties.getMaxPoolSize(), poolProperties.getQueueCapacity());
         }
         ThreadPoolTaskExecutor executor = new VisiableThreadPoolTaskExecutor();
-        executor.setCorePoolSize(corePoolSize);
-        executor.setMaxPoolSize(maxPoolSize);
-        executor.setQueueCapacity(queueCapacity);
-        executor.setThreadNamePrefix(threadNamePrefix);
+        executor.setCorePoolSize(poolProperties.getCorePoolSize());
+        executor.setMaxPoolSize(poolProperties.getMaxPoolSize());
+        executor.setQueueCapacity(poolProperties.getQueueCapacity());
+        executor.setThreadNamePrefix(poolProperties.getThreadNamePrefix());
 
         // rejection-policy：当pool已经达到max size的时候，如何处理新任务
         // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
