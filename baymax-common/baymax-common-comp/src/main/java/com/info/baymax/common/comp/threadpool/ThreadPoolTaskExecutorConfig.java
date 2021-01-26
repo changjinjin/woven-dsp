fba245b9ae3249a3f4b808e6828bd516b8834147
760374564 <760374564@qq.com>
2021-01-26 20:02:07 +0800

    feat(common): add thread pool properties
    add thread pool properties
    
    (cherry picked from commit 56c9bcd3f028e6d12e702b67db8573cb22f7e77f)

--- baymax-common/baymax-common-comp/src/main/java/com/info/baymax/common/comp/threadpool/ThreadPoolTaskExecutorConfig.java
+++ baymax-common/baymax-common-comp/src/main/java/com/info/baymax/common/comp/threadpool/ThreadPoolTaskExecutorConfig.java
@@ -36,6 +36,8 @@ public class ThreadPoolTaskExecutorConfig {
         executor.setMaxPoolSize(poolProperties.getMaxPoolSize());
         executor.setQueueCapacity(poolProperties.getQueueCapacity());
         executor.setThreadNamePrefix(poolProperties.getThreadNamePrefix());
+        executor.setKeepAliveSeconds(poolProperties.getKeepAliveSeconds());
+        executor.setAllowCoreThreadTimeOut(poolProperties.isAllowCoreThreadTimeOut());
 
         // rejection-policy：当pool已经达到max size的时候，如何处理新任务
         // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
