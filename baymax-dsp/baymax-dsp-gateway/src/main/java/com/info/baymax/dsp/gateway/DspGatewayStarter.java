973dfec2fd410e269b2edd7c7ae8401d52a15707
760374564 <760374564@qq.com>
2021-03-16 17:49:56 +0800

    feat(docker): add docker profile

--- baymax-dsp/baymax-dsp-gateway/src/main/java/com/info/baymax/dsp/gateway/DspGatewayStarter.java
+++ baymax-dsp/baymax-dsp-gateway/src/main/java/com/info/baymax/dsp/gateway/DspGatewayStarter.java
@@ -1,16 +1,14 @@
 package com.info.baymax.dsp.gateway;
 
+import com.info.baymax.dsp.gateway.config.YamlPropertySourceFactory;
+import com.merce.woven.metrics.config.EnableElasticMetricsExport;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
-
-import com.info.baymax.dsp.gateway.config.YamlPropertySourceFactory;
-import com.merce.woven.metrics.config.EnableElasticMetricsExport;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
@@ -21,8 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @PropertySource(value = {"classpath:/dsp-gateway.yml"}, factory = YamlPropertySourceFactory.class)
 public class DspGatewayStarter {
     public static void main(String[] args) {
-        // @formatter:off
-    	log.info("JAVA CLASS PATH = " + System.getProperty("java.class.path"));
+        log.info("JAVA CLASS PATH = " + System.getProperty("java.class.path"));
 		SpringApplication.run(DspGatewayStarter.class, args);
     }
 }
