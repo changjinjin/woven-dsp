9332c6fe919620d200afcb344570e34cfe2cfe2b
changjinjin <1445571720@qq.com>
2021-06-16 19:00:11 +0800

    fix(ip):change IP address authentication rules

--- baymax-dsp/baymax-dsp-access/baymax-dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/config/ClientRequestFilter.java
+++ baymax-dsp/baymax-dsp-access/baymax-dsp-access-dataapi/src/main/java/com/info/baymax/dsp/access/dataapi/config/ClientRequestFilter.java
@@ -1,6 +1,9 @@
 package com.info.baymax.dsp.access.dataapi.config;
 
+import com.info.baymax.common.utils.ICollections;
+import com.info.baymax.common.utils.crypto.RSAGenerater;
 import com.info.baymax.dsp.access.dataapi.utils.IPAddressUtils;
+import com.info.baymax.dsp.data.consumer.entity.DataCustApp;
 import com.info.baymax.dsp.data.consumer.service.DataCustAppService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -28,33 +32,68 @@ public class ClientRequestFilter implements WebFilter {
     @Autowired
     private DataCustAppService dataCustAppService;
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-
-        ServerHttpRequest request = exchange.getRequest();
+    private boolean ipAddressVerification(ServerHttpRequest request) {
+        HttpHeaders headers = request.getHeaders();
         String ipAddress = IPAddressUtils.getIpAddress(request);
 
-        String path = request.getURI().getPath();
-        if (
-                path.startsWith("/api/dsp/dataapi/data/pullRecords")
-                || path.startsWith("/api/dsp/dataapi/data/pullAggs")
-//                || path.startsWith("/api/dsp/dataapi/data/pullBySql")
-//                || path.startsWith("/api/dsp/dataapi/data/secertkey")
-                || path.startsWith("/api/dsp/dataapi/data/pullBySqlRemote")
-        ) {
-            // 处理请求主机信息
-            HttpHeaders headers = request.getHeaders();
-            List<String> userIds = headers.get("userId");
-            String userId = null;
-            if (userIds != null && !userIds.isEmpty()) {
-                userId = userIds.get(0);
+        List<String> accessKeys = headers.get("accessKey");
+        if(null != accessKeys && accessKeys.size() > 0){
+            String accessKey = accessKeys.get(0);
+            log.info("accessKey = " + accessKey);
+            DataCustApp dataCustApp = dataCustAppService.selectByAccessKeyNotNull(accessKey);
+            String[] accessIps = dataCustApp.getAccessIp();
+            List<String> ipList = Arrays.asList(accessIps);
+            if (ipList.contains(ipAddress)) {
+                return true;
+            }else{
+                return false;
             }
-            List<String> ipList = dataCustAppService.selectAccessIps(userId);
-            if(!ipList.contains(ipAddress)){
+        }else {
+            throw new RuntimeException("accessKey cannot be null");
+        }
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+
+        if(path.indexOf("pullBySqlRemote") != -1
+                || path.indexOf("pullRecordsRemote") != -1
+                || path.indexOf("pullAggsRemote") != -1){
+
+            boolean flag = ipAddressVerification(request);
+            if (true == flag) {
+                boolean b = accessTokenVerification(request);
+                if(b == true){
+                    return chain.filter(exchange);
+                }else{
+                    throw new RuntimeException("Wrong accessToken");
+                }
+            } else {
                 throw new RuntimeException("Wrong accessIp");
             }
+        }else{
             return chain.filter(exchange);
         }
-        return chain.filter(exchange);
+    }
+
+    private boolean accessTokenVerification(ServerHttpRequest request) {
+        HttpHeaders headers = request.getHeaders();
+        List<String> accessKeys = headers.get("accessKey");
+        List<String> accessTokens = headers.get("accessToken");
+        if(ICollections.hasNoElements(accessKeys)){
+            log.info("accessKey为空");
+            throw new RuntimeException("accessKey cannot be null");
+        }
+        if(ICollections.hasNoElements(accessTokens)){
+            log.info("accessToken为空");
+            throw new RuntimeException("accessToken cannot be null");
+        }
+        String accessKey = accessKeys.get(0);
+        String accessToken = accessTokens.get(0);
+        DataCustApp app = dataCustAppService.selectByAccessKeyNotNull(accessKey);
+        String generateRandomPassword = RSAGenerater.decryptByPublicKey(accessToken, app.getPublicKey());
+        return generateRandomPassword.startsWith(accessKey);
     }
 }
